45f770bb43526f11ef53195f4c525ecbbdd9eabb
Rodrigo Leguizamón <rodrigoleguizamon09@gmail.com>
2020-01-08 15:48:07 -0300

    modificaciones-08-01-20

--- routes/carrito.js
+++ routes/carrito.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/carrito', function(req, res) {
+router.get('/', function(req, res) {
     res.render('carrito')
   });
 
--- routes/detalle.js
+++ routes/detalle.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/detalle', function(req, res) {
+router.get('/', function(req, res) {
     res.render('detalle-producto')
   });
 
