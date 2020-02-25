16a87af8606495fbf17ad37fa2400f449aadc770
lusanj <57303304+lusanj@users.noreply.github.com>
2020-02-25 01:28:59 -0300

    commit-base-25-02-2020

--- structure.sql
+++ structure.sql
@@ -1,19 +1,21 @@
-CREATE DATABASE 'green_db'
+CREATE DATABASE IF NOT EXISTS 'green_db';
 
 CREATE TABLE 'users' (
-  'userId' smallint(6) NOT NULL,
+  'id' smallint(6) NOT NULL,
   'mail' varchar(95) NOT NULL,
   'password' varchar(95) NOT NULL,
-  'foto' VARBINARY(MAX) DEFAULT NULL,
+  'foto' varchar(255) DEFAULT NULL,
   'createAt' timestamp NOT NULL,
   'updateAt' timestamp DEFAULT NULL,
   PRIMARY KEY ('id')
 );
 
 CREATE TABLE 'carrito' (
-  'carritoId' smallint(6) NOT NULL,
+  'id' smallint(6) NOT NULL,
   'userId' int(6) NOT NULL,
-  'vacio' varchar(3) NOT NULL,
+  'precioTotal' decimal(5,2) NOT NULL,
+  'status' varchar(10) NOT NULL,
+  'date' timestamp NOT NULL,
   PRIMARY KEY ('id')
 );
 
@@ -22,25 +24,16 @@ CREATE TABLE 'productos' (
   'categoria' varchar(85) NOT NULL,
   'nombre' varchar(85) NOT NULL,
   'descripcion' varchar(255) DEFAULT NULL,
-  'foto' VARBINARY(MAX) DEFAULT NULL,
+  'foto' varchar(255) DEFAULT NULL,
   'precioUnitario' decimal(3,2) NOT NULL,
   'fechaUpdate' timestamp NOT NULL,
   'cantidadStock' smallint(6) NOT NULL,
   PRIMARY KEY ('id')
 );
 
-CREATE TABLE 'ordenes' (
-  'ordenId' smallint(6) NOT NULL,
-  'carritoId' smallint(6) NOT NULL,
-  'precioTotal' decimal(5,2) NOT NULL,
-  'status' varchar(10) NOT NULL,
-  'dateOrden' timestamp NOT NULL,
-  PRIMARY KEY ('ordenId')
-);
-
-CREATE TABLE 'producto_orden' (
+CREATE TABLE 'producto_carrito' (
   'id' smallint(6) NOT NULL,
-  'ordenId' smallint(6) NOT NULL,
+  'carritoId' smallint(6) NOT NULL,
   'productoId' smallint(6) NOT NULL,
   'cantidad' smallint(6) NOT NULL,
   'precioUnitario' decimal(3,2) DEFAULT NULL,
