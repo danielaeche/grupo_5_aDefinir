1bedc2e5cea88ea9ae26c47bc166d0000bd6ec94
lusanj <57303304+lusanj@users.noreply.github.com>
2020-03-20 16:53:07 -0300

    models-20200320

---
+++ database/models/carrito.js
@@ -0,0 +1,56 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = "carrito";
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        userId: {
+            type: dataTypes.STRING,
+            allowNull: false
+        },
+        precioTotal: {
+            type: dataTypes.INTEGER,
+            allowNull: true
+        },
+        status: {
+            type: dataTypes.STRING,
+            allowNull: true
+        },
+        date: {
+            type: dataTypes.timestamps,
+            allowNull: false
+        }
+    }
+    let config = {
+        tableName: "carrito",
+        underscored: true,
+        timestamps: true
+    }
+
+
+const carrito = sequelize.define(alias,cols,config);
+/* relacion con productos */
+carrito.associate = function(models){
+    carrito.belongsTo(models.productos,{
+        as: "productos",
+        throught: "producto_carrito",
+        foreignKey: "productoId",
+        otherKey: "carritoId",
+        timestamps: false
+    });
+}
+
+/* relacion con users */
+carrito.associate = function(models){
+    carrito.belongsTo(models.users,{
+        as: "usuario",
+        foreignKey: "userId"
+    });
+} 
+
+return carrito;
+
+}
