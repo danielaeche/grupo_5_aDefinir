5c9a09b5be3d1560cd4b187f723358dd4e97bfe8
lusanj <57303304+lusanj@users.noreply.github.com>
2020-04-17 14:36:27 -0300

    upload-2020-04-17

--- routes/carrito.js
+++ routes/carrito.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var carritoControllers= require ('../controllers/carritoControllers');
-var authMiddleware = require('../middlewares/authMiddleware')
-
+var authMiddleware = require('../middlewares/authMiddleware');
 
 router.get('/', authMiddleware, carritoControllers.index);
 
+// listar los productos del carrito
+router.get('/', carritoControllers.buscarCarrito);
+
+//agregar productos al carrito
+router.post('/', carritoControllers.agregarProducto);
+
+//borrar un producto
+router.delete('/:id',carritoControllers.eliminarProducto);
 
 module.exports = router;
