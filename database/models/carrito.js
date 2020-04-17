26b3593357e9ecdca67d8970961f16bf1bbf3b25
lusanj <57303304+lusanj@users.noreply.github.com>
2020-04-17 14:39:05 -0300

    upload-2020-04-17

--- database/models/carrito.js
+++ database/models/carrito.js
@@ -1,56 +1,17 @@
-module.exports = (sequelize, dataTypes) => {
-    let alias = "carrito";
-    let cols = {
-        id: {
-            type: dataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false
-        },
-        userId: {
-            type: dataTypes.STRING,
-            allowNull: false
-        },
-        precioTotal: {
-            type: dataTypes.INTEGER,
-            allowNull: true
-        },
-        status: {
-            type: dataTypes.STRING,
-            allowNull: true
-        },
-        date: {
-            type: dataTypes.timestamps,
-            allowNull: false
-        }
-    }
-    let config = {
-        tableName: "carrito",
-        underscored: true,
-        timestamps: true
-    }
+var express = require('express');
+var router = express.Router();
+var carritoControllers= require ('../controllers/carritoControllers');
+var authMiddleware = require('../middlewares/authMiddleware');
 
+router.get('/', authMiddleware, carritoControllers.index);
 
-const carrito = sequelize.define(alias,cols,config);
-/* relacion con productos */
-carrito.associate = function(models){
-    carrito.belongsTo(models.productos,{
-        as: "productos",
-        throught: "producto_carrito",
-        foreignKey: "productoId",
-        otherKey: "carritoId",
-        timestamps: false
-    });
-}
+// listar los productos del carrito
+router.get('/', carritoControllers.buscarCarrito);
 
-/* relacion con users */
-carrito.associate = function(models){
-    carrito.belongsTo(models.users,{
-        as: "usuario",
-        foreignKey: "userId"
-    });
-} 
+//agregar productos al carrito
+router.post('/', carritoControllers.agregarProducto);
 
-return carrito;
+//borrar un producto
+router.delete('/:id',carritoControllers.eliminarProducto);
 
-}
+module.exports = router;
