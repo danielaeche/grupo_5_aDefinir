318d5231f16834acd98dfab7083a651862e5a60a
lusanj <57303304+lusanj@users.noreply.github.com>
2020-04-17 14:21:47 -0300

    upload-2020-04-17

--- controllers/carritoControllers.js
+++ controllers/carritoControllers.js
@@ -1,6 +1,40 @@
+let db = require('../database/models');
+
 const carritoControllers= {
     index: function(req, res) {
         res.render('carrito')},
+
+    buscarCarrito: function(req,res){
+        db.carrito.findByPk(req,session.carrito).then(data =>{
+            res.json({carrito: data})
+        })
+
+    },
+
+    agregarProducto: function(req,res){
+        //levantar id y cantidad producto
+        let {productoId: prodId, qty} = req.body;
+        //buscar producto en db
+        let producto = await db.producto.findByPk(prodId);        
+        //buscar el carrito en session
+        //guardar el producto en el carrito
+        db.carrito.findByPk(req.session.carrito).then(carrito => {
+            carrito.addItem(producto,{ through: {
+                cantidad: qty,
+                precio: producto.precio
+            }})
+            res.redirect('/carrito');
+        })
+    },
+
+    eliminarProducto: function(req,res){
+        db.carrito.destroy({
+            where: {
+            productoId: req.params.producto,
+            carritoId: req.session.carrito
+        }})
+        res.redirect('/');
+    }
 
 }
 module.exports= carritoControllers;
