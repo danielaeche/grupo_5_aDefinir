4d12f24e3b74f0ebc006aceab6171d80cd8e8f04
rodrigoleguizamon <rodrigoleguizamon09@gmail.com>
2020-04-17 21:32:48 -0300

    se crean sesiones para usuarios no logueados

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret:"frase secreta", resave:true}));
+app.use(session({secret:"frase secreta", resave:false, saveUninitialized:false}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
